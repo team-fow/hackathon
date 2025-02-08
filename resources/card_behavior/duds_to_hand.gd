8311d7b67310ab6716c311309fe95666a2621471
sagealtman <jaltman01@wesleyan.edu>
2025-02-08 16:39:21 -0500

    sends more duds to hand

--- resources/card_behavior/duds_to_hand.gd
+++ resources/card_behavior/duds_to_hand.gd
@@ -6,5 +6,4 @@ func play(game: Node) -> void:
 	for i: int in 2:
 		var card = load("res://scenes/card/card.tscn").instantiate()
 		card.card_resource = load("res://resources/card_data/meta/dud.tres")
-		game.get_opponent(card.player).hand.add_card(card) 
-		game.get_opponent(card.player).deck.shuffle()
+		game.get_opponent(card.player)._send_card_to_hand(card) 
