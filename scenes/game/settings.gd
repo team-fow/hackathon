600b5abe026254f478851702a6c559caddfcca6a
webcatzz <june.webcatz@gmail.com>
2025-02-09 10:55:35 -0500

    setting screen is in

--- scenes/game/settings.gd
+++ scenes/game/settings.gd
@@ -13,3 +13,21 @@ func _on_continue_pressed() -> void:
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_music_value_changed(value: float) -> void:
+	AudioServer.set_bus_volume_db(1, linear_to_db(value))
+
+
+func _on_sfx_value_changed(value: float) -> void:
+	AudioServer.set_bus_volume_db(2, linear_to_db(value))
+
+
+func _on_texture_progress_bar_gui_input(event: InputEvent) -> void:
+	if event.is_action_pressed("click") or event is InputEventMouse and Input.is_action_pressed("click"):
+		$Panel/Margins/VBox/TextureProgressBar.value = event.position.x / $Panel/Margins/VBox/TextureProgressBar.get_rect().size.x
+
+
+func _on_texture_progress_bar_2_gui_input(event: InputEvent) -> void:
+	if event.is_action_pressed("click") or event is InputEventMouse and Input.is_action_pressed("click"):
+		$Panel/Margins/VBox/TextureProgressBar2.value = event.position.x / $Panel/Margins/VBox/TextureProgressBar2.get_rect().size.x
