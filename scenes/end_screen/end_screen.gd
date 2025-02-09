2af1509ecaebe4154d830fd2bf5457bbe316418c
webcatzz <june.webcatz@gmail.com>
2025-02-09 14:09:04 -0500

    end screens

--- scenes/end_screen/end_screen.gd
+++ scenes/end_screen/end_screen.gd
@@ -11,11 +11,19 @@ func set_winner(winner: Player, loser: Player) -> void:
 		$VBoxContainer/GridContainer/Label3.text = "With a temperature of %s." % winner.temperature
 		$VBoxContainer/GridContainer/Label4.text = "%s llost..." % loser.player_name
 		$VBoxContainer/GridContainer/Label5.text = "With a temperature of %s." % loser.temperature
+		
+		if randf() > 0.5: $Hot.show()
+		else: $Cold.show()
 	else:
 		$VBoxContainer/GridContainer/Label2.text = "%s won!" % winner.player_name
 		$VBoxContainer/GridContainer/Label3.text = "With a temperature of %s." % winner.temperature
 		$VBoxContainer/GridContainer/Label4.text = "%s lost..." % loser.player_name
 		$VBoxContainer/GridContainer/Label5.text = "With a temperature of %s." % loser.temperature
+		
+		if loser.temperature > 0:
+			$Hot.show()
+		else:
+			$Cold.show()
 
 
 func _on_button_pressed() -> void:
